68fc91e39ca3cdc07432d4dbf846c2573b07e746
Glitch (set-the-pace) <none>
2025-05-27 10:31:23 +0000

    🟠🕑 Updated with Glitch

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticDev = "set_PACE"
+const service_worker_version = "v1.011"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/footprint.txt",
+  "/script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
